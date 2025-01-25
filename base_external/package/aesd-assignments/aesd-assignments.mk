ddf451dff91b23ebead3826ec9c339fd94b68e0d
comerts <comerts@gmail.com>
2025-01-25 17:23:23 +0300

    Assingment 3 commit version updated

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 99e6ab7248624b4ef2a8544b819a84bb91a7842a
+AESD_ASSIGNMENTS_VERSION = 00892f35d9265e24e3e5aa35d37dea972a18ded5
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
